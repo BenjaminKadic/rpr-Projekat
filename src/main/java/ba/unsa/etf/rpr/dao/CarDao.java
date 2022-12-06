2c4136506eb29077af19f21d018b6644c75a3a10
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 13:10:17 +0100

    added searchByTransmission

--- src/main/java/ba/unsa/etf/rpr/dao/CarDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarDao.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Car;
+import ba.unsa.etf.rpr.domain.Transmission;
+
+import java.util.List;
 
 public interface CarDao extends Dao<Car>{
+    List<Car> searchByTransmission(Transmission transmission);
 }
