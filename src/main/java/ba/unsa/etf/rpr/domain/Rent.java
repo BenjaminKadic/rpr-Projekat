69dfff7076b92cfe1939f033f8dc20021bfb993d
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 13:00:59 +0100

    added getters and setters for Rent class

--- src/main/java/ba/unsa/etf/rpr/domain/Rent.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Rent.java
@@ -8,4 +8,44 @@ public class Rent {
     private User user;
     private Date startDate;
     private Date endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
