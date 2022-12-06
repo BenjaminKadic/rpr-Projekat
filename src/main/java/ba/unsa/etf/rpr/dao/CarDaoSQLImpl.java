1a424e5f92f9b98921ad738b49195d01c5ba3d2b
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 19:09:27 +0100

    added methods to CarDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/CarDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarDaoSQLImpl.java
@@ -1,4 +1,80 @@
 package ba.unsa.etf.rpr.dao;
 
-public class CarDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Car;
+import ba.unsa.etf.rpr.domain.Color;
+import ba.unsa.etf.rpr.domain.Fuel;
+import ba.unsa.etf.rpr.domain.Transmission;
+
+import java.util.List;
+
+public class CarDaoSQLImpl implements CarDao{
+    @Override
+    public List<Car> searchByTransmission(Transmission transmission) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByColor(Color color) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByMakeYearRange(int start, int end) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByHorsepower(int horsepower) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByFuel(Fuel fuel) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByMake(String make) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByModel(String model) {
+        return null;
+    }
+
+    @Override
+    public List<Car> searchByPriceRange(int min, int max) {
+        return null;
+    }
+
+    @Override
+    public boolean checkIfRented(Car car) {
+        return false;
+    }
+
+    @Override
+    public Car getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Car add(Car item) {
+        return null;
+    }
+
+    @Override
+    public Car update(Car item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return null;
+    }
 }
