c93e983d07cf789943e61007fcad4c4b818d0f4f
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 16:11:34 +0100

    added getByDateRange

--- src/main/java/ba/unsa/etf/rpr/dao/RentDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentDao.java
@@ -2,5 +2,9 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Rent;
 
+import java.util.Date;
+import java.util.List;
+
 public interface RentDao extends Dao<Rent>{
+    List<Rent> getByDateRange(Date start, Date end);
 }
