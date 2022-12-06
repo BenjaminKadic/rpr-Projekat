b921497aa70124feb91e87af6da5ef05c7a0aca5
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 12:57:05 +0100

    override of equals and hashCode for User class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class User {
     private int id;
     private String username;
@@ -79,5 +81,18 @@ public class User {
         return "User " +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
