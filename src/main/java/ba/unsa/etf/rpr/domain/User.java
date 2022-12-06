28912694a35fb01d2d1750dec7ac360e8d3f6c7a
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 12:51:04 +0100

    Added attributes to User class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.domain;
 
 public class User {
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String mail;
+    private String phoneNumber;
 }
