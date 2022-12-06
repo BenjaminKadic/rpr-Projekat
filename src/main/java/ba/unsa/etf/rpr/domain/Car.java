7f1134133a75919cc5bca17b6efc1e00ea0a7ca8
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 09:53:07 +0100

    Added Car class

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Car.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.domain;
+
+public class Car {
+    private int id;
+    private String make;
+    private String model;
+    private String color;
+    private String registration;
+    private int make_year;
+    private int price;
+    private boolean rented;
+    private String fuel;
+    private String transmission;
+    private int mileage;
+    private int horsepower;
+
+}
