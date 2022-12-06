d375acd37d2e357ed1ddb7d819e72f4021d24597
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-06 13:11:08 +0100

    added searchByColor

--- src/main/java/ba/unsa/etf/rpr/dao/CarDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarDao.java
@@ -1,10 +1,13 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Car;
+import ba.unsa.etf.rpr.domain.Color;
 import ba.unsa.etf.rpr.domain.Transmission;
 
 import java.util.List;
 
 public interface CarDao extends Dao<Car>{
     List<Car> searchByTransmission(Transmission transmission);
+    List<Car> searchByColor(Color color);
+
 }
