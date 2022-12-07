1db381b2e1ecb78675b619b6f14ca708c8f94f7d
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 11:39:30 +0100

    added javadoc comments to UserDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -2,5 +2,10 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.User;
 
+/**
+ * Dao interface for User domain bean
+ *
+ * @author Benjamin Kadic
+ */
 public interface UserDao extends Dao<User>{
 }
