b2a75b2db1a8c0800bc8fbc0066ce95d62971f49
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 20:04:20 +0100

    Added methods to UserDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.dao;
 
-public class UserDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.User;
+
+import java.util.List;
+
+public class UserDaoSQLImpl implements UserDao{
+    @Override
+    public User getById(int id) {
+        return null;
+    }
+
+    @Override
+    public User add(User item) {
+        return null;
+    }
+
+    @Override
+    public User update(User item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
 }
