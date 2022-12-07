262ee5c324f495a5c0c872f25b87a4220e621057
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 20:07:16 +0100

    Added constructor and attribute to UserDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,10 +1,20 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Car;
 import ba.unsa.etf.rpr.domain.User;
 
+import java.sql.*;
 import java.util.List;
 
 public class UserDaoSQLImpl implements UserDao{
+    private Connection connection;
+    public UserDaoSQLImpl() {
+        try {
+            this.connection= DriverManager.getConnection("", "", "");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public User getById(int id) {
         return null;
