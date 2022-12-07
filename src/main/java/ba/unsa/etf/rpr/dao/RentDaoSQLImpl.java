4e3b651d607ef6e7c042b0f55897d5f2b64b125c
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 20:50:38 +0100

    implemented add

--- src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
@@ -41,8 +41,47 @@ public class RentDaoSQLImpl implements RentDao{
         return null;
     }
 
+    /**
+     * returns next id for adding rents to database
+     *
+     * @return int value of next id
+     */
+    private int getMaxId(){
+        int id=1;
+        String query = "SELECT MAX(id)+1 FROM Rents";
+        try {
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()) {
+                id = rs.getInt(1);
+                rs.close();
+                return id;
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem pri radu sa bazom podataka");
+            System.out.println(e.getMessage());
+        }
+        return id;
+    }
     @Override
     public Rent add(Rent item) {
+        int id = getMaxId();
+        String query = "INSERT INTO Rents VALUES (?,?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query );
+            item.setId(id);
+            stmt.setInt(1, item.getId());
+            stmt.setInt(2,item.getCar().getId());
+            stmt.setInt(3,item.getUser().getId());
+            stmt.setBoolean(4, item.isReturned());
+            stmt.setDate(5,item.getStartDate());
+            stmt.setDate(6, item.getEndDate());
+            stmt.executeUpdate();
+            return item;
+        } catch (SQLException e) {
+            System.out.println("Problem pri radu sa bazom podataka");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
