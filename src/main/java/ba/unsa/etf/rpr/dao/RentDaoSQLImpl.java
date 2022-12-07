6dea4f79b70b7e1cf1ee7d7479da673d8893cf72
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 20:45:19 +0100

    implemented getById

--- src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
@@ -2,8 +2,7 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Rent;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Date;
 import java.util.List;
 
@@ -18,6 +17,27 @@ public class RentDaoSQLImpl implements RentDao{
     }
     @Override
     public Rent getById(int id) {
+        String query = "SELECT * FROM Rents WHERE id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){ // result set is iterator.
+                Rent rent = new Rent();
+                rent.setId(rs.getInt("id"));
+                rent.setCar(new CarDaoSQLImpl().getById(rs.getInt("car_id")));
+                rent.setUser(new UserDaoSQLImpl().getById(rs.getInt("user_id")));
+                rent.setReturned(rs.getBoolean("returned"));
+                rent.setStartDate(rs.getDate("start"));
+                rent.setEndDate(rs.getDate("end"));
+                rs.close();
+                return rent;
+            }else{
+                return null; // if there is no elements in the result set return null
+            }
+        }catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
         return null;
     }
 
