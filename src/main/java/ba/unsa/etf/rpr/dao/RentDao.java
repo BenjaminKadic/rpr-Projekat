72f3adef322b426abce19693c9443d24aaa1d607
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 11:39:14 +0100

    added javadoc comments to RentDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/RentDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentDao.java
@@ -5,6 +5,18 @@ import ba.unsa.etf.rpr.domain.Rent;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Dao interface for Rent domain bean
+ *
+ * @author Benjamin Kadic
+ */
+
 public interface RentDao extends Dao<Rent>{
+    /**
+     * Search rents in database based on date range
+     * @param start start date
+     * @param end end date
+     * @return List of rents from rents table
+     */
     List<Rent> getByDateRange(Date start, Date end);
 }
