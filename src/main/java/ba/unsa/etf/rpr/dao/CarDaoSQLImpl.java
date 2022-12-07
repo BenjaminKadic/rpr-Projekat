da57f1886f6e74a53e2a67f55b6edf67601a3b4a
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 09:36:22 +0100

    implemented getById

--- src/main/java/ba/unsa/etf/rpr/dao/CarDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarDaoSQLImpl.java
@@ -4,10 +4,20 @@ import ba.unsa.etf.rpr.domain.Car;
 import ba.unsa.etf.rpr.domain.Color;
 import ba.unsa.etf.rpr.domain.Fuel;
 import ba.unsa.etf.rpr.domain.Transmission;
+import jdk.jfr.Category;
 
+import java.sql.*;
 import java.util.List;
 
 public class CarDaoSQLImpl implements CarDao{
+    private Connection connection;
+    public CarDaoSQLImpl() {
+        try {
+            this.connection= DriverManager.getConnection("", "", "");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public List<Car> searchByTransmission(Transmission transmission) {
         return null;
@@ -55,6 +65,21 @@ public class CarDaoSQLImpl implements CarDao{
 
     @Override
     public Car getById(int id) {
+        String query = "SELECT * FROM Cars WHERE id = ?";
+        try{
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){ // result set is iterator.
+                Car car = new Car();
+                car.setId(rs.getInt("id"));
+                car.setMake(rs.getString("make"));
+            }else{
+                return null; // if there is no elements in the result set return null
+            }
+        }catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
         return null;
     }
 
