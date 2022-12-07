a162dd9cbc96a3e708746c4bb992c8fc4ad125e7
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-07 19:38:15 +0100

    added attribute, constructor and methods

--- src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
@@ -1,4 +1,48 @@
 package ba.unsa.etf.rpr.dao;
 
-public class RentDaoSQLImpl {
+import ba.unsa.etf.rpr.domain.Rent;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Date;
+import java.util.List;
+
+public class RentDaoSQLImpl implements RentDao{
+    private Connection connection;
+    public RentDaoSQLImpl() {
+        try {
+            this.connection= DriverManager.getConnection("", "", "");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public Rent getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Rent add(Rent item) {
+        return null;
+    }
+
+    @Override
+    public Rent update(Rent item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Rent> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Rent> getByDateRange(Date start, Date end) {
+        return null;
+    }
 }
