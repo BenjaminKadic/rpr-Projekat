3f2545aa0c3b9863fe3b049849be5eab6bc3b2d4
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-10 17:51:22 +0100

    implemented file reading

--- src/main/java/ba/unsa/etf/rpr/dao/DBConnection.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DBConnection.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DBConnection {
+    static Properties props = new Properties();
+    FileInputStream in;
+
+    {
+        try {
+            in = new FileInputStream("src/main/resources/DBConnection.properties");
+            props.load(in);
+            in.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Problemi pri pristupu file-u");
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
