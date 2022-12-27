5701ac740ab117a9d7411b4945ec7976255aa202
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-27 19:25:29 +0100

    Update of carmanager class

--- src/main/java/ba/unsa/etf/rpr/business/CarManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CarManager.java
@@ -3,15 +3,19 @@ package ba.unsa.etf.rpr.business;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Car;
 import ba.unsa.etf.rpr.exceptions.RentACarException;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarManager {
     public List<Car> getAll() throws RentACarException {
         return DaoFactory.carDao().getAll();
     }
 
-    public List<Car> searchCars(String text) throws RentACarException {
-        List<Car> cars=DaoFactory.carDao().searchByModel(text);
+    public Set<Car> searchCars(String text) throws RentACarException {
+        Set<Car> cars=new HashSet<>();
+        cars.addAll(DaoFactory.carDao().searchByModel(text));
         cars.addAll(DaoFactory.carDao().searchByMake(text));
         return cars;
     }
