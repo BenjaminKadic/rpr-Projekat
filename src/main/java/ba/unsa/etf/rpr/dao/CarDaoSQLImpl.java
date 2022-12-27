4685975650b91f8cb2a7a083cecd66e383c9d4fc
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-27 15:46:13 +0100

    used executeQuery method

--- src/main/java/ba/unsa/etf/rpr/dao/CarDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/CarDaoSQLImpl.java
@@ -5,7 +5,6 @@ import ba.unsa.etf.rpr.domain.Color;
 import ba.unsa.etf.rpr.exceptions.RentACarException;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -51,89 +50,30 @@ public class CarDaoSQLImpl extends AbstractDao<Car> implements CarDao{
     @Override
     public List<Car> searchByColor(Color color) throws RentACarException {
         String query = "SELECT * FROM Cars WHERE color = ?";
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            stmt.setString(1, color.value);ResultSet rs = stmt.executeQuery();
-            ArrayList<Car> cars = new ArrayList<>();
-            while (rs.next()) {
-                cars.add(row2object(rs));
-            }
-            rs.close();
-            return cars;
-        } catch (SQLException e) {
-            throw new RentACarException(e.getMessage(), e);
-        }
+        return executeQuery(query, new Object[]{color.value});
     }
 
     @Override
     public List<Car> searchByMake(String make) throws RentACarException {
         String query = "SELECT * FROM Cars WHERE UPPER(make) LIKE concat('%', ?, '%')";
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            stmt.setString(1, make.toUpperCase());
-            ResultSet rs = stmt.executeQuery();
-            ArrayList<Car> cars = new ArrayList<>();
-            while (rs.next()) {
-                cars.add(row2object(rs));
-            }
-            rs.close();
-            return cars;
-        } catch (SQLException e) {
-            throw new RentACarException(e.getMessage(), e);
-        }
+        return executeQuery(query, new Object[]{make.toUpperCase()});
     }
 
     @Override
     public List<Car> searchByModel(String model) throws RentACarException {
         String query = "SELECT * FROM Cars WHERE UPPER(model) LIKE concat('%', ?, '%')";
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            stmt.setString(1, model.toUpperCase());
-            ResultSet rs = stmt.executeQuery();
-            ArrayList<Car> cars = new ArrayList<>();
-            while (rs.next()) {
-                cars.add(row2object(rs));
-            }
-            rs.close();
-            return cars;
-        } catch (SQLException e) {
-            throw new RentACarException(e.getMessage(), e);
-        }
+        return executeQuery(query, new Object[]{model.toUpperCase()});
     }
 
     @Override
     public List<Car> searchByPriceRange(int min, int max) throws RentACarException {
         String query = "SELECT * FROM Cars WHERE price BETWEEN ? and ?";
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            stmt.setInt(1, min);
-            stmt.setInt(2, max);
-            ResultSet rs = stmt.executeQuery();
-            ArrayList<Car> cars = new ArrayList<>();
-            while (rs.next()) {
-                cars.add(row2object(rs));
-            }
-            rs.close();
-            return cars;
-        } catch (SQLException e) {
-            throw new RentACarException(e.getMessage(), e);
-        }
+        return executeQuery(query, new Object[]{min,max});
     }
 
     @Override
     public List<Car> searchAvailable() throws RentACarException {
         String query = "SELECT * FROM car c WHERE c.id NOT IN (SELECT r.car_id FROM Rents WHERE r.returned = 0)";
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            ArrayList<Car> cars = new ArrayList<>();
-            while (rs.next()) {
-                cars.add(row2object(rs));
-            }
-            rs.close();
-            return cars;
-        } catch (SQLException e) {
-            throw new RentACarException(e.getMessage(), e);
-        }
+        return executeQuery(query, new Object[]{});
     }
 }
