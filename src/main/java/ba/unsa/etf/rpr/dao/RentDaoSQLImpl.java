084ee90b5038bdcf3e3ba84a5b86a938e2eb9934
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-27 15:46:32 +0100

    used executeQuery method

--- src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentDaoSQLImpl.java
@@ -4,7 +4,6 @@ import ba.unsa.etf.rpr.domain.Rent;
 import ba.unsa.etf.rpr.exceptions.RentACarException;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -46,21 +45,6 @@ public class RentDaoSQLImpl extends AbstractDao<Rent> implements RentDao{
     @Override
     public List<Rent> getByDateRange(Date start, Date end) throws RentACarException{
         String query = "SELECT * FROM Rents WHERE start BETWEEN ? and ? AND end BETWEEN ? and ?";
-        ArrayList<Rent> rents = new ArrayList<>();
-        try{
-            PreparedStatement stmt = getConnection().prepareStatement(query);
-            stmt.setObject(1, start);
-            stmt.setObject(2, end);
-            stmt.setObject(3, start);
-            stmt.setObject(4, end);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                rents.add(row2object(rs));
-            }
-            rs.close();
-            return rents;
-        }catch (SQLException e){
-            throw new RentACarException(e.getMessage(),e);
-        }
+        return executeQuery(query, new Object[]{start, end, start, end});
     }
 }
