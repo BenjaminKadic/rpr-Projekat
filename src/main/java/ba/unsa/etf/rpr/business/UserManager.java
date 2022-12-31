2a9cde7c83156eb58d5ae0931b6740ae7d4850a0
BenjaminKadic <bkadic1@etf.unsa.ba>
2022-12-31 04:10:53 +0100

    created a class UserManager

---
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.RentACarException;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Business Logic Layer for management of Users
+ *
+ * @author Benjamin Kadic
+ */
+public class UserManager {
+
+}
