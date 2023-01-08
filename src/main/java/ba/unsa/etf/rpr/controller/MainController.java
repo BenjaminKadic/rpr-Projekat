7692b88527b39f1b87909a3889f27102d0adb030
BenjaminKadic <bkadic1@etf.unsa.ba>
2023-01-08 01:29:06 +0100

    added Model for Car

--- src/main/java/ba/unsa/etf/rpr/controller/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/MainController.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.controller;
 
+import ba.unsa.etf.rpr.domain.Car;
 import ba.unsa.etf.rpr.domain.User;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +12,7 @@ import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
@@ -36,19 +39,43 @@ public class MainController {
             new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
         }
     }
+    protected class CarModel {
+        public SimpleStringProperty make = new SimpleStringProperty("");
+        public SimpleStringProperty model = new SimpleStringProperty("");
+        public SimpleStringProperty color = new SimpleStringProperty("");
+        public SimpleStringProperty registration = new SimpleStringProperty("");
+        public SimpleIntegerProperty price = new SimpleIntegerProperty();
+        public void fromCar(Car car){
+            this.make.set(car.getMake());
+            this.model.set(car.getModel());
+            this.color.set(car.getColor());
+            this.registration.set(car.getRegistration());
+            this.price.set(car.getPrice());
+        }
+        public Car toCar(){
+            Car car = new Car();
+            car.setMake(this.make.getValue());
+            car.setModel(this.model.getValue());
+            car.setColor(this.color.getValue());
+            car.setRegistration(this.registration.getValue());
+            car.setPrice(this.price.getValue());
+            return car;
+        }
+    }
+
     protected class UserModel {
         public SimpleStringProperty username = new SimpleStringProperty("");
         public SimpleStringProperty firstName = new SimpleStringProperty("");
         public SimpleStringProperty lastName = new SimpleStringProperty("");
         public SimpleStringProperty password = new SimpleStringProperty("");
-        public SimpleObjectProperty<Date> birthdate = new SimpleObjectProperty<>();
+        public SimpleObjectProperty<LocalDate> birthdate = new SimpleObjectProperty<>();
 
-        public void fromQuote(User user){
+        public void fromUser(User user){
             this.username.set(user.getUsername());
             this.firstName.set(user.getFirstName());
             this.lastName.set(user.getLastName());
             this.password.set(user.getPassword());
-            this.birthdate.set(user.getBirthdate());
+            this.birthdate.set(user.getBirthdate().toLocalDate());
         }
 
         public User toUser(){
@@ -57,7 +84,7 @@ public class MainController {
             user.setFirstName(this.firstName.getValue());
             user.setLastName(this.lastName.getValue());
             user.setPassword(this.password.getValue());
-            user.setBirthdate(this.birthdate.getValue());
+            user.setBirthdate(Date.valueOf(this.birthdate.getValue()));
             return user;
         }
     }
