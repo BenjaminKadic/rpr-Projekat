d138766df44219a8b8deaf9fa9bf4206b47f2125
BenjaminKadic <bkadic1@etf.unsa.ba>
2023-02-20 16:54:02 +0100

    added database dump

---
+++ DB dump.sql
@@ -0,0 +1,115 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: sql.freedb.tech    Database: freedb_RentACar
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cars`
+--
+
+DROP TABLE IF EXISTS `Cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Cars` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `make` varchar(45) NOT NULL,
+  `model` varchar(45) NOT NULL,
+  `color` varchar(45) NOT NULL,
+  `registration` varchar(7) NOT NULL,
+  `price` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `registration_UNIQUE` (`registration`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cars`
+--
+
+LOCK TABLES `Cars` WRITE;
+/*!40000 ALTER TABLE `Cars` DISABLE KEYS */;
+INSERT INTO `Cars` VALUES (1,'Volkswagen','Golf','White','A12O345',120),(2,'Volkswagen','Polo','Blue','J37A953',70),(3,'Audi','A6','Blue','T21M740',180),(4,'Hyundai','Santa Fe','Black','E35O953',180),(5,'Chevrolet','Lacetti','Red','A53J239',120),(6,'Ford','Focus','Blue','O20K324',90),(7,'Fiat','Punto ','White','M78A539',50),(8,'Skoda','Rapid ','White','A87J832',140),(9,'Hyundai','Elantra','Black','E48A283',140),(10,'Ford ','Kuga ','Black','J23E003',150),(11,'Volkswagen','Golf','Red','A78E923',90),(12,'Ford','Mustang','Red','A01E001',240),(15,'Hyundai','i30','Blue','A23E739',70);
+/*!40000 ALTER TABLE `Cars` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Rents`
+--
+
+DROP TABLE IF EXISTS `Rents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Rents` (
+  `id` int NOT NULL,
+  `car_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `start` date NOT NULL,
+  `end` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Car_id_idx` (`car_id`),
+  KEY `User_id_idx` (`user_id`),
+  CONSTRAINT `car_id` FOREIGN KEY (`car_id`) REFERENCES `Cars` (`id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Rents`
+--
+
+LOCK TABLES `Rents` WRITE;
+/*!40000 ALTER TABLE `Rents` DISABLE KEYS */;
+INSERT INTO `Rents` VALUES (1,4,1,'2022-12-01','2022-12-03'),(2,3,3,'2022-11-09','2022-11-14'),(3,2,9,'2023-02-11','2023-03-01');
+/*!40000 ALTER TABLE `Rents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `license` varchar(45) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `birthdate` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`license`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'123456','Benjamin','Kadic','2001-12-21'),(2,'764521','Eman','Alibalic','2002-09-30'),(3,'876345','Amar','Tahirovic','2001-12-21'),(4,'098743','Merima','Duric','2002-08-26'),(5,'927423','Aida','Zametica','2001-12-14'),(6,'639563','Lejla','Heleg','2001-12-11'),(9,'376346','Eldar','Muratovic','2002-07-10');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-20 16:51:42
